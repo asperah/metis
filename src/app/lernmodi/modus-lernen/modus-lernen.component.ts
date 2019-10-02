615082b05ff724863ac28ecc9486444b0cdec467
asperah <asperahdevel@gmail.com>
2019-10-02 13:26:32 +0200

    Version 1.4.0 Filtern klappt, Options klappen, müssen zusammen gefügt werden

--- src/app/lernmodi/modus-lernen/modus-lernen.component.ts
+++ src/app/lernmodi/modus-lernen/modus-lernen.component.ts
@@ -15,7 +15,8 @@ export class ModusLernenComponent implements OnInit {
 questionsf: Question[];
 rc1: String[];
 resultF: string[];
-questions: string[];
+questions: Question[];
+miep: Number[] = [1,2,3];
 
   constructor(private oqs: OptionsQuestService,
               private qs: QuestionService
@@ -28,22 +29,29 @@ questions: string[];
   this.rc1 = this.resultF[0].split(',', 6);
   alert(`${this.rc1[0]},${this.rc1[1]},${this.rc1[2]},${this.rc1[3]},${this.rc1[4]},${this.rc1[5]}`);
   this.getQuestions();
+  console.log(this.questionsf);
 
-   
+
+
 
 
   }
 
     getQuestions (){    
-    this.qs.getAll().subscribe(res => this.questionsf = res
-     .filter(item => item.QCatalogue == 1)      
-      .filter(item => item.QCatalogue ==  2)
-      .filter(item => item.QCatalogue == 3)
-     .filter(item => item.QType == 1)
-      .filter(item => item.QType == 2)
-      .filter(item => item.QType == 3));
     
+    this.qs.getAll().subscribe(res => this.questionsf = res      
+     .filter(item => (item.QCatalogue ==  this.miep[0])||(item.QCatalogue ==  this.miep[1])||(item.QCatalogue ==  this.miep[2]))
+     .filter(item => (item.QType ==  this.miep[0])||(item.QType ==  this.miep[1])||(item.QType ==  this.miep[2]))
+     );
 
+    /*
+      .filter(item => item.QCatalogue == this.miep[1])      
+      .filter(item => item.QCatalogue ==  this.miep[1])
+      .filter(item => item.QCatalogue == this.miep[1])
+      .filter(item => item.QType == this.miep[0])
+      .filter(item => item.QType == this.miep[0])
+      .filter(item => item.QType == this.miep[0])));
+  */
   }
 }
 
